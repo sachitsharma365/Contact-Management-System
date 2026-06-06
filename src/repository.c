20debe549a477358d9ffc776a94eea7bd4a61281
Sachit Sharma <sachitsharma365@gmail.com>
2026-06-06 12:57:17 +0530

    Fixed some bugs in repository.c

--- src/repository.c
+++ src/repository.c
@@ -8,6 +8,12 @@ static int contactCount = 0;
 
 void addContact()
 {
+  if (contactCount >= 100)
+  {
+    printf("Contact list is full.\n");
+    return;
+  }
+
   char tempName[100], tempPhone[20], tempAddress[100], tempEmail[100];
 
   // TAKING USER INPUT
@@ -30,17 +36,19 @@ void addContact()
   printf("\n%s %s %s %s\n", tempName, tempPhone, tempAddress, tempEmail);
 
   // STORING USER INPUT IN contacts ARRAY
-  strncpy(contacts[contactCount].name, tempName, sizeof(contacts[contactCount].name - 1));
-  contacts[contactCount].name[sizeof(contacts[contactCount].name - 1)] = '\0';
+  contacts[contactCount].id = contactCount + 1;
 
-  strncpy(contacts[contactCount].phone, tempPhone, sizeof(contacts[contactCount].phone - 1));
-  contacts[contactCount].phone[sizeof(contacts[contactCount].phone - 1)] = '\0';
+  strncpy(contacts[contactCount].name, tempName, sizeof(contacts[contactCount].name) - 1);
+  contacts[contactCount].name[sizeof(contacts[contactCount].name) - 1] = '\0';
 
-  strncpy(contacts[contactCount].address, tempAddress, sizeof(contacts[contactCount].address - 1));
-  contacts[contactCount].address[sizeof(contacts[contactCount].address - 1)] = '\0';
+  strncpy(contacts[contactCount].phone, tempPhone, sizeof(contacts[contactCount].phone) - 1);
+  contacts[contactCount].phone[sizeof(contacts[contactCount].phone) - 1] = '\0';
 
-  strncpy(contacts[contactCount].email, tempEmail, sizeof(contacts[contactCount].email - 1));
-  contacts[contactCount].email[sizeof(contacts[contactCount].email - 1)] = '\0';
+  strncpy(contacts[contactCount].address, tempAddress, sizeof(contacts[contactCount].address) - 1);
+  contacts[contactCount].address[sizeof(contacts[contactCount].address) - 1] = '\0';
+
+  strncpy(contacts[contactCount].email, tempEmail, sizeof(contacts[contactCount].email) - 1);
+  contacts[contactCount].email[sizeof(contacts[contactCount].email) - 1] = '\0';
 
   contactCount++;
 }
